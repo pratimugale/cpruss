c9516724156b3ed4d9b2700c771d4d9bcd713ca6
Pratim Ugale <pratim.ugale@gmail.com>
2019-05-28 20:20:16 +0000

    test

---
+++ test/code/test.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void){
+    printf("Started executing!\n");
+
+    char command[200];
+    strcpy(command, "touch setup1.sh");
+    system(command);
+    strcpy(command, "echo #!/bin/bash > setup1.sh");
+    system(command);
+    strcpy(command, "echo export PRUN=0 > setup1.sh");
+    system(command);
+    strcpy(command, "echo export TARGET=hello > setup1.sh");
+    system(command);
+    strcpy(command, "chmod +x setup1.sh");
+    system(command);
+    strcpy(command, "source setup1.sh");
+    system(command);
+    strcpy(command, "echo PRUN=$PRUN");
+    system(command);
+    strcpy(command, "echo TARGET=$TARGET");
+    system(command);
+
+    strcpy(command, "echo none > /sys/class/leds/beaglebone\:green\:usr3/trigger");
+    system(command);
+
+    printf("Completed execution!\n");
+
+    return 0;
+}
--- test/test.c
+++ test/test.c
@@ -3,14 +3,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Function prototypes
+void start();
+
+char command[200];
+
 int main(void){
     printf("Started executing!\n");
 
-    char command[200];
-    strcpy(command, "ls");
-    system(command);
-
+    start();
     printf("Completed execution!\n");
 
     return 0;
+}
+
+void start(){
+    //Procedure to Start the PRU execution(on whichever firmware is currently loaded)
+    //PRU needs to be stopped before using this function again.
+
+    strcpy(command, "echo start | sudo tee -a /sys/devices/platform/ocp/4a326004.pruss-soc-bus/4a300000.pruss/4a334000.pru/remoteproc/remoteproc1/state");
+    system(command);
+
+    
 }
