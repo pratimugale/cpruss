bdc1071de03d5eb400ec90e03a75a13134363aea
Pratim Ugale <pratim.ugale@gmail.com>
2019-05-30 09:58:24 +0000

    state() function works

--- test/cpruss.c
+++ test/cpruss.c
@@ -7,13 +7,14 @@
 void start();
 void stop();
 void restart();
+void state();
 
 char command[150];
 
 int main(void){
     printf("Started executing!\n");
-
-    restart();
+    //restart();
+    state();
     printf("Completed execution!\n");
 
     return 0;
@@ -48,11 +49,26 @@ void start(){
     system(command);
 }
 void stop(){
+    //Procedure to Stop the PRU execuion(on whichever firmware is currently being executed)
     strcpy(command, "echo stop | sudo tee -a /sys/devices/platform/ocp/4a326004.pruss-soc-bus/4a300000.pruss/4a334000.pru/remoteproc/remoteproc1/state");
     system(command);
 }
 void restart(){
-    //For all practical purposes, use restart() instead of start
+    //Procedure to stop the current execution and start again.(executes same firmware again)
+    //For all practical purposes, restart() is better to use than start()
     stop();
     start();
+}
+void state(){
+    //Returns (as a string), the current state of the PRU
+
+    //state = "abcdefg\0";
+    //return strdup(&state[0]);
+    FILE *file = fopen("/sys/devices/platform/ocp/4a326004.pruss-soc-bus/4a300000.pruss/4a334000.pru/remoteproc/remoteproc1/state", "r"); 
+    if (file != NULL){
+        char status[8];
+        fgets(status, sizeof status, file);
+        printf("%s", status);
+        printf("%i", strcmp(status, "offline"));
+    }
 }
