f8457f85ee35b60cacfad9b9588703d4fd7ecfe0
Pratim Ugale <pratim.ugale@gmail.com>
2019-05-30 17:40:46 +0000

    pru 2 also added

--- libraries/cpruss.c
+++ libraries/cpruss.c
@@ -10,17 +10,20 @@
 void start(int n);
 void stop(int n);
 void restart(int n);
-void state(int n);
+char* state(int n);
+char status[8];
 
 char command[150];
 int prun;
 
+// main() function is for temporary testing 
 int main(void){
     printf("Started executing!\n");
     //restart();
     prun = 1;
-    restart(prun);
-    state();
+    start(prun);
+    stop(2);
+    state(2);
     printf("Completed execution!\n");
 
     return 0;
@@ -52,36 +55,65 @@ void start(int n){
         }
     }*/
     if(n == 1){
-        strcpy(command, "echo start | sudo tee -a /sys/devices/platform/ocp/4a326004.pruss-soc-bus/4a300000.pruss/4a334000.pru/remoteproc/remoteproc1/state");
-        system(command);
+        // strcmp() returns 0 if both strings DO match!
+        if (strcmp(state(1), "running")){
+            strcpy(command, "echo start | sudo tee -a /sys/devices/platform/ocp/4a326004.pruss-soc-bus/4a300000.pruss/4a334000.pru/remoteproc/remoteproc1/state");
+            system(command);
+        }
+        else{
+            printf("PRU 1 is currently running and start() function cannot be executed.\n"); 
+        }
     }
     else if (n == 2){
-        strcpy(command, "echo start | sudo tee -a /sys/devices/platform/ocp/4a326004.pruss-soc-bus/4a300000.pruss/4a338000.pru/remoteproc/remoteproc2/state");
-        system(command);
+        if (strcmp(state(2), "running")){
+             strcpy(command, "echo start | sudo tee -a /sys/devices/platform/ocp/4a326004.pruss-soc-bus/4a300000.pruss/4a338000.pru/remoteproc/remoteproc2/state");
+             system(command);
+        }
+        else{
+            printf("PRU 2 is currently running and start() function cannot be executed.\n"); 
+        }
     }
     else{
-        printf("invalid prun input in function start()");
+        printf("Invalid PRUn input in function start()");
     }
 }
 void stop(int n){
     //Procedure to Stop the PRU execuion(on whichever firmware is currently being executed)
 
     if (n == 1){
-        strcpy(command, "echo stop | sudo tee -a /sys/devices/platform/ocp/4a326004.pruss-soc-bus/4a300000.pruss/4a334000.pru/remoteproc/remoteproc1/state");
-        system(command);
+        if (strcmp(state(1), "offline")){
+            strcpy(command, "echo stop | sudo tee -a /sys/devices/platform/ocp/4a326004.pruss-soc-bus/4a300000.pruss/4a334000.pru/remoteproc/remoteproc1/state");
+            system(command);
+        }
+        else{
+            printf("PRU 1 is already offline and cannot be stop()ped.\n");
+        }
     }
     else if (n == 2){
-        strcpy(command, "echo stop | sudo tee -a /sys/devices/platform/ocp/4a326004.pruss-soc-bus/4a300000.pruss/4a338000.pru/remoteproc/remoteproc2/state");
-        system(command);
+        if (strcmp(state(2), "offline")){
+            strcpy(command, "echo stop | sudo tee -a /sys/devices/platform/ocp/4a326004.pruss-soc-bus/4a300000.pruss/4a338000.pru/remoteproc/remoteproc2/state");
+            system(command);
+        }
+        else{
+            printf("PRU 2 is already offline and cannot be stop()ped.\n");
+        }
+    }
+    else{
+        printf("Invalid PRUn input in function stop()");
     }
 }
 void restart(int n){
     //Procedure to stop the current execution and start again.(executes same firmware again)
     //For all practical purposes, restart() is better to use than start()
-    stop(n);
-    start(n);
+    if (n == 1 || n == 2){ 
+        stop(n);
+        start(n);
+    }
+    else{
+        printf("Invalid PRUn input provided in function restart()");
+    }
 }
-void state(int n){
+char* state(int n){
     //Returns (as a string), the current state of the PRU
 
     //state = "abcdefg\0";
@@ -90,18 +122,22 @@ void state(int n){
     if (n == 1){
         FILE *file = fopen("/sys/devices/platform/ocp/4a326004.pruss-soc-bus/4a300000.pruss/4a334000.pru/remoteproc/remoteproc1/state", "r"); 
         if (file != NULL){
-            char status[8];
             fgets(status, sizeof status, file);
-            printf("Status of PRU 1: %s", status);
+            return status;
+            //printf("Status of PRU 1: %s", status);
         }
     }
     else if (n == 2){
         FILE *file = fopen("/sys/devices/platform/ocp/4a326004.pruss-soc-bus/4a300000.pruss/4a338000.pru/remoteproc/remoteproc2/state", "r"); 
         if (file != NULL){
-            char status[8];
             fgets(status, sizeof status, file);
-            printf("Status of PRU 2: %s", status);
+            return status;
+            //printf("Status of PRU 2: %s", status);
+        }
+        else{
+             printf("Invalid PRUn input in function state()");
         }
 
     }
+
 }
